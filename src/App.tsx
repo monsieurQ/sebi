f1e0e72a4306530659ab5bbbed42ff1a4bacc69c
Quirin Schneider <quirschnei@gmail.com>
2023-10-19 04:14:52 +0200

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { Routes, Route, HashRouter }
+import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import { Home } from './home';
 import Sebibox from './pages/sebibox';
@@ -9,13 +9,13 @@ import { Login } from './pages/Login';
 function App() {
   
   return (
-    <HashRouter>
+    <Router>
       <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/sebibox' element={<Sebibox />} />
           <Route path='/sebibox/login' element={<Login />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
